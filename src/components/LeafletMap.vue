088c0950c12a357e2006e3543ffae34299e65dc4
Ulf Treger <ulf.treger@3plusx.de>
2024-08-19 14:47:37 +0200

    Layer control with overlays, clustered

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -38,69 +38,54 @@ export default {
     }
 
     const addDataToMap = (data) => {
-      // Create layers and add data to the map
-      const layer1 = L.featureGroup()
-      const layer2 = L.featureGroup()
-
-      data.features.forEach((feature) => {
-        const marker = L.marker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
-
-        const popupContent = `
-            <h3>${feature.properties.name}</h3>
-            <p>${feature.properties.description}</p>
-            <img src="${feature.properties.image}" alt="${feature.properties.name}" style="max-width: 100px;">
-          `
-
-        marker.bindPopup(popupContent)
-
-        if (feature.properties.layer === 'layer1') {
-          layer1.addLayer(marker)
-        } else {
-          layer2.addLayer(marker)
-        }
-      })
-
-      // plain FeatureGroups
-      // layer1.addTo(map.value)
-      // layer2.addTo(map.value)
-
       // Create a MarkerClusterGroup
       const markers = L.markerClusterGroup()
+      // Create Metalayer object
+      const featureGroups = {}
+      // Create layers and add data to the map
+      console.log(data)
+      data.map.layer.forEach((layer) => {
+        let layer_group = L.markerClusterGroup()
 
-      // Transfer markers from FeatureGroup to MarkerClusterGroup
-      layer2.eachLayer((layer) => {
-        if (layer instanceof L.Marker) {
-          markers.addLayer(layer)
-        }
+        layer.places.forEach((place) => {
+          const marker = L.marker([place.lat, place.lon])
+
+          const popupContent = `
+              <p class="place-layer">${layer.title}</p>
+              <p class="place-address">${place.location} ${place.address} ${place.city}</p>
+              <h3>${place.title}</h3>
+              <p>${place.teaser}</p>
+            `
+
+          marker.bindPopup(popupContent)
+
+          layer_group.addLayer(marker)
+        })
+
+        // Transfer markers from FeatureGroup to MarkerClusterGroup
+        layer_group.eachLayer((layer) => {
+          if (layer instanceof L.Marker) {
+            markers.addLayer(layer)
+          }
+        })
+        // map.value.addLayer(markers)
+        featureGroups[layer.title] = layer_group
+        map.value.addLayer(layer_group)
       })
-      map.value.addLayer(markers)
 
       console.log('bounds per default')
       let bounds = [53.55, 9.95]
 
-      // Combine the bounds of all LayerGroups
-      if (layer1.getLayers().length && layer2.getLayers().length) {
-        console.log('both layers')
-        bounds = L.latLngBounds(layer1.getBounds()).extend(layer2.getBounds())
-        // Fit the map to the combined bounds
-        map.value.fitBounds(bounds)
-      } else if (layer2.getLayers().length) {
-        console.log('only layer 2')
-        bounds = L.latLngBounds(layer2.getBounds())
-      }
+      bounds = L.latLngBounds(markers.getBounds())
+
+      // featureGroups['All layers'] = markers
+
       console.log(bounds)
       const padding = 0.1 // 10% padding
       const paddedBounds = bounds.pad(padding)
       map.value.fitBounds(paddedBounds)
 
-      const layerControl = L.control.layers(
-        {
-          'Layer 1': layer1,
-          'Layer 2': layer2
-        },
-        null,
-        { collapsed: false }
-      )
+      const layerControl = L.control.layers(null, featureGroups, { collapsed: true })
       layerControl.addTo(map.value)
     }
 
@@ -109,6 +94,21 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 @import 'leaflet/dist/leaflet.css';
+
+h3 {
+  font-weight: bold;
+}
+
+p.place-layer {
+  display: inline-block;
+  background-color: lightsalmon;
+  padding: 2px 5px;
+  margin: 0;
+  border-radius: 5px;
+}
+p.place-address {
+  margin: 3px 0;
+}
 </style>
