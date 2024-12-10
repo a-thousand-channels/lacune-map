366b7aa03c5a785fcc575f3afd99a45e2fca7383
Ulf Treger <ulf.treger@3plusx.de>
2024-12-10 15:22:04 +0100

    OG metadata

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,73 +1,34 @@
 <template>
   <div id="about" class="content">
     <h1>Lacune </h1>
+    <p>Ein Projekt für mehr Sichbarkeit und Erinnerung</p>
     <h2>Impressum</h2>
     <p>
       Heike Schader<br>
-      <a href="mailto:">E-Mail HIER!</a><br>
+      <a href="mailto:info@lacune-map.de">info@lacune-map.de</a><br>
       Anschrift: HIER<br>
+      Straße,<br/>
+      PLZ Ort
     </p>
     <p>
       Die Produktion dieser Karte wurde durch A Thousand Channels unterstützt.
       A Thousand Channels ist eine queere Mapping Platform. A Thousand Channels sind Thomas Böker, Dafne Jaramillo, Ulf Treger<br />
-      @a-thousand-channels (Insta etc)
+      <a href="https://linktr.ee/a_thousand.channels" target='blank'>Website</a> & 
+      <a href="https://www.instagram.com/a_thousand_channels/" target='blank'>@a_thousand_channels</a>
     </p>
       
     <p>
+      <em>
       Die dunkle Hintergrundkarte wurde unter Verwendung von Geodaten von Openstreetmap und Mitwirkenden erstellt. Die historischen Hamburg-Karten aus den 1920er bis 1980er Jahren werden vom Landesbetrieb Geoinformation und Vermessung der Stadt Hamburg unter einer "Deutschland Namensnennung 2.0" Datenlizenz zur Verfügung gestellt.
+    </em>
     </p>
 
     <hr />
     <h2>Datenschutz</h2>  
     <p>...</p>
-      <hr />
-    <h2>TODOs</h2>
-    <p>Was erledigt ist, in Arbeit ist oder noch gemacht werden muss:</p>
-    <ul>
-      <li><strike>Timeslider Design</strike></li>
-      <li><strike>Clusterfärbung nach den Markern dieses Clusters</strike></li>
-      <li><strike>Kartenfehler/Clusterprobleme fixen</strike></li>
-      <li><strike>Eigenen Kartenhintergrund einbauen</strike></li>
-      <li><strike>Layer Infoseite mit Bild (verlinkt aus den Popups)</strike></li>
-      <li><strike>Historical Maps 20er, 30er etc</strike></li>
-      <li><strike>Inhaltseite zu einem Orte (Link aus den Popups)</strike></li>
-      <li><strike>Einzelansicht als Overlay links neben der Karte</strike></li>
-      <li><strike>Einzelansicht hübscher setzen.</strike></li>
-      <li><strike>Timeslider Funktionalität</strike></li>
-      <li><strike>Bilder</strike></li>
-      <li><strike>Loader-Kreisel bis alle Daten geladen sind.</strike></li>
-      <li><strike>Relations einbauen.</strike></li>
-      <li><strike>Schöneres Layer-Auswahl-Menu oben rechts mit Farbe</strike></li>
-      <li><strike>In Arbeit: Deeplinks für einzelne Orte!</strike></li>
-      <li><strike>Link aus der Layerinfoseite zu einem Ort fliegt an die entsprechende Stelle und öffnet das Popup</strike></li>
-      <li>In Arbeit: Deeplink für Layer</li>
-      <li>TODO: Datumsangaben verbessern, zb. 1923-1923</li>
-      <li><strike>Bug: Filtern nach Layern und Timeline funktioniert nicht richtig</strike></li>
-      <li><strike>Bug: Cluster aus vergangenen Einträgen wird nicht weiß angezeigt.</strike></li>
-      <li><strike>Bug: Darkmode Karte wird manchmal über die historischen Karten gelegt</strike></li>
-      <li><strike>Bug: Sidebar wechsel im MAC</strike></li>
-      <li><strike>Timeline beschränken von 1920-1990</strike></li>
-      <li><strike>Bug: Peformance verbessern</strike></li>
-      <li><strike>Bug: Peformance verbessern</strike></li>
-      <li>Bug: Cluster aufklappen per Klick aus der Seitenleiste</li>
-      <li>Bug: </li>
-      <li>Impressum/Datenschutz/Credits Seite?</li>
-    </ul>
-    <br/>
-    <p>Fragen</p>
-    <ul>
-      <li>Einträge in Gewässern besser ohne Adressangaben, oder?</li>
-    </ul>
-    <br/>
-    <p>TODOs vielleicht einen Tick später</p>
-    <ul>
-      <li>Timeline mit Mengenangaben, also je Jahr eine verdickte Timeline um sehen zu können wie die Verteilung ist</li>
-      <li>OpenGraph Angaben für Social Media</li>
+    <hr />
 
-    </ul>
-    <br/>
-    <p>Letzte Änderungen: https://github.com/a-thousand-channels/lacune-map/commits/main/</p>
-    <p>&nbsp;</p>
+
   </div>
 </template>
 
