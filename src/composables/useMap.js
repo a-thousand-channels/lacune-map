ba9e51a1a0756449ac9ebd13dbb6960f6d9b4080
Ulf Treger <ulf.treger@3plusx.de>
2024-12-11 17:10:05 +0100

    Fix nasty bug on open-a-popup-and-scroll which destroys updating all marker positions. yeah. plus some unneeded code removal

--- src/composables/useMap.js
+++ src/composables/useMap.js
@@ -1,6 +1,5 @@
 // composables/usemapElement.js
-import { shallowRef, ref, reactive, provide, inject } from 'vue'
-import L from 'leaflet'
+import { shallowRef, ref, provide, inject } from 'vue'
 
 
 export const useMap = () => {
@@ -8,8 +7,6 @@ export const useMap = () => {
   // const mapInstance = inject(mapInstanceSymbol, ref(null))
   const mapInstance = inject(mapInstanceSymbol, shallowRef(null))
   const isMapReady = ref(false)
-  const markersRegistrySymbol = Symbol('markersRegistry')
-  const markersRegistry = reactive(inject(markersRegistrySymbol, new Map()))  
 
   const defaultCenter = [53.56, 10.01]
   const savedCenter = localStorage.getItem('mapCenter')
@@ -37,72 +34,25 @@ export const useMap = () => {
 
     // Provide the map instance and markers registry to child components
     provide(mapInstanceSymbol, mapInstance)
-    provide(markersRegistrySymbol, markersRegistry)
 
     return mapInstance.value
+
+  
   }
 
 
-
-  const registerMarker = (id, coordinates, popupContent = '') => {
-    if (isMapReady.value && mapInstance.value) {      
-        // console.log('registerMarker', id)
-        if (markersRegistry.has(id)) {
-          console.warn(`Marker with ID ${id} already exists`)
-          return
-        }
-
-        const marker = L.marker(coordinates)
-         // .addTo(mapInstance.value)
-        
-        if (popupContent) {
-          marker.bindPopup(popupContent)
-        }
-
-        markersRegistry.set(id, marker)
-        // console.log('Marker registered:', id, marker)
-        // console.log('markersRegistry', markersRegistry.size)
-      }
-  }
 
   const focusMarkerById = (id, zoom = 18) => {
     console.log('focusMarkerById', id)
-    console.log('markersRegistry', markersRegistry.size)
     console.log('isMapReady', isMapReady.value)
     console.log('mapInstance', mapInstance.value) 
-    if (isMapReady.value && mapInstance.value) {   
-      console.log('markersRegistry', markersRegistry.size)
-      const marker = markersRegistry.get(id)
-      if (!marker) {
-        console.warn(`No marker found for ID: ${id}`)
-        return
-      }
-      console.log('marker found', marker)
-
-      marker.setStyle?.({ color: '#ff3333' })
-      const latLng = marker.getLatLng()
-      mapInstance.value.setView(latLng, zoom)
-    }
   }
 
-  const removeMarker = (id) => {
-    ensureMapReady(() => {
-      const marker = markersRegistry.get(id)
-      if (marker) {
-        marker.remove()
-        markersRegistry.delete(id)
-      }
-    })
-  }
-
+ 
   return {
     mapInstance,
     isMapReady,
-    initMap,
-    registerMarker,
-    focusMarkerById,
-    removeMarker,
-    markersRegistry
+    initMap
   }
 };
 
