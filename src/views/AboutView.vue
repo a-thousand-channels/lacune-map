f2e07da4e16b0b0aaf3c472533fccfcb80f3d1fd
Ulf Treger <ulf.treger@3plusx.de>
2024-12-14 19:15:53 +0100

    Imprint

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,22 +5,22 @@
     <h2>Impressum</h2>
     <p>
       Heike Schader<br>
-      <a href="mailto:info@lacune-map.de">info@lacune-map.de</a><br>
-      Anschrift: HIER<br>
-      Straße,<br/>
-      PLZ Ort
+      E-Mail: <a href="mailto:info@lacune-map.de">info@lacune-map.de</a><br>
+      Anschrift: c/o Bildwechsel Kultur- und Medienzentrum für Frauen e.V., Kirchenallee 25, 20099 Hamburg
     </p>
     <p>
       Die Produktion dieser Karte wurde durch A Thousand Channels unterstützt.
       A Thousand Channels ist eine queere Mapping Platform. A Thousand Channels sind Thomas Böker, Dafne Jaramillo, Ulf Treger<br />
-      <a href="https://linktr.ee/a_thousand.channels" target='blank'>Website</a> & 
-      <a href="https://www.instagram.com/a_thousand_channels/" target='blank'>@a_thousand_channels</a>
+      <a href="https://linktr.ee/a_thousand.channels" target='blank'>A Thousand Channels Website</a> & 
+      <a href="https://www.instagram.com/a_thousand_channels/" target='blank'>@a_thousand_channels bei Instagram</a>
     </p>
       
     <p>
       <em>
-      Die dunkle Hintergrundkarte wurde unter Verwendung von Geodaten von Openstreetmap und Mitwirkenden erstellt. Die historischen Hamburg-Karten aus den 1920er bis 1980er Jahren werden vom Landesbetrieb Geoinformation und Vermessung der Stadt Hamburg unter einer "Deutschland Namensnennung 2.0" Datenlizenz zur Verfügung gestellt.
-    </em>
+      Die dunkle Hintergrundkarte wurde für dieses Projekt gestaltet und basiert auf Geodaten von <a href="https://www.openstreetmap.org/copyright" target="_blank">Openstreetmap und Mitwirkenden</a>. 
+      <br />
+      Die historischen Hamburg-Karten aus den 1920er bis 1980er Jahren werden vom Landesbetrieb Geoinformation und Vermessung der Stadt Hamburg unter einer "<a href="https://www.govdata.de/dl-de/by-2-0" target="_blank">Deutschland Namensnennung 2.0</a>" Datenlizenz als <a href="https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000?REQUEST=GetCapabilities&SERVICE=WMS" target="_blank">Web Map Service (WMS)</a> zur Verfügung gestellt.
+      </em>
     </p>
 
     <hr />
