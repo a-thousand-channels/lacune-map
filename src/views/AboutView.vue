0658723c888e94ae7b357531101dcd19d4d7fabc
Ulf Treger <ulf.treger@3plusx.de>
2024-12-22 20:10:22 +0100

    Adding privacy statement

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -14,6 +14,7 @@
       <a href="https://linktr.ee/a_thousand.channels" target='blank'>A Thousand Channels Website</a> & 
       <a href="https://www.instagram.com/a_thousand_channels/" target='blank'>@a_thousand_channels bei Instagram</a>
     </p>
+    <p>Dieses Angebot ist ein privates Informationsangebot, es wurde ehrenamtlich erstellt. Es bestehen keine Vermarktungsabsichten</p>
       
     <p>
       <em>
@@ -24,8 +25,69 @@
     </p>
 
     <hr />
-    <h2>Datenschutz</h2>  
-    <p>...</p>
+  
+    <h2>Datenschutzerklärung</h2> 
+    <p>Diese Erklärung beschreibt die Aspekte des Datenschutz für die Website https://lacune-map.de</p>
+    <p>
+      Verantwortlich für diese Website ist:<br/>
+      Heike Schader     <br />
+      E-Mail: info@lacune-map.de<br />
+      Postanschrift: Lacune, Eckermannstraße 4, 21423 Winsen <br />
+    </p>  
+    <p>Diese Seite wurde durch die Unterstützung des Projekts A Thousand Channels realisiert und veröffentlicht</p>
+
+    <h3>Datenerfassung und -verarbeitung</h3>
+    
+
+    <p>Die Website wird mit einer TLS Verschlüsselung übertragen.</p>
+    <p>
+      Bei einem Besuch der Website werden von uns selbst keine Daten erfasst oder abgefragt und insofern auch nicht verarbeitet. </p>
+    <p>
+      Für die Bereitstellung dieser Website nutzen wir eine Dienstleistung, "Hosting" genannt, die diese Website ausliefert. </p>
+    <p> 
+      Um den Zugriff auf die Informationen dieser Seite in Form eines Stadtplans zu ermöglichen, haben wir verschiedene Hintergrundkarten integriert, um die Orte im Stadtraum von Hamburg heute bzw. in der jeweiligen Zeit lokalisieren zu können. Wir nutzen hierfür externe Dienste, da diese Karten aus sehr umfangreichen Daten bestehen, besondere Dienste für ihre Bereitstellung benötigen bzw. nur über den genutzten Weg verfügbar sind.
+    </p>
+
+    <h3>Hosting der Website</h3>
+    <p>
+      Die Website wird extern bei folgendem Anbieter gehostet:
+      Uberspace vertreten durch Jonas Pasche, Kaiserstr. 15, 55116 Mainz</p>
+    <p>
+      A Thousand Channels hat einen Vertrag über Auftragsverarbeitung abgeschlossen. Dieser Vertrag stellt sicher, dass personenbezogene Daten der Besucher*innen nur nach Anweisung und unter Einhaltung der DSVGO erfolgt. Das Logging auf diesem Server ist allerdings deaktiviert, Aufrufe der Website werden dort nicht protokolliert. 
+    </p>
+
+    <h3>
+      Hosting der Hintergrundkarten
+    </h3>
+    <p>
+      Die Hintergrundkarte "Aktuelle Hamburg Karte (Dark)" wird extern auf einem Server bei folgendem Anbieter gehostet:
+      3plusx vertreten durch Ulf Treger, Gaußstraße 25b, 22765 Hamburg
+    </p>
+    <p>
+      A Thousand Channels hat haben einen Vertrag mit 3plusx über Auftragsverarbeitung abgeschlossen. Dieser Vertrag stellt sicher, dass personenbezogene Daten der Besucher*innen nur nach Anweisung und unter Einhaltung der DSVGO erfolgt. Das Logging auf diesem Server ist allerdings deaktiviert, daher werden die Aufrufe der Hintergrundkarte dort nicht protokolliert. 
+    </p>
+    <p>
+      Die historischen Hintergundkarten werden extern über eine Webmap-Service durch einen Server der 
+      Freien und Hansestadt Hamburg bereitgetellt. Zuständig für diesen Service ist der 
+      Landesbetrieb Geoinformation und Vermessung (LGV) Hamburg, Neuenfelder Straße 19, D-21109 Hamburg
+      Zum Umgang der Daten, die bei Benutzung dieses Service erhoben werden, siehe bitte die Datenschutzerklärung des Geoportals des LGV: 
+      https://geoportal-hamburg.de/resources/Datenschutzerkl%C3%A4rung_Masterportal_Rollout.pdf
+    </p>
+
+    <h3>Beschwerderecht</h3>
+
+    <p>Euch steht das Recht auf Beschwerde zu. Zuständig hierfür ist die Datenschutzbehörde der Stadt Hamburg:</p>
+
+    <p>
+      Der Hamburgische Beauftragte für Datenschutz und Informationsfreiheit<br />
+      Ludwig-Erhard-Str. 22, 20459 Hamburg<br />
+      Tel.: (040) 4 28 54 - 40 40<br />
+      E-Fax: (040) 4 279 – 11811<br />
+      E-Mail: mailbox@datenschutz.hamburg.de
+    </p>
+    <p>
+      Stand dieser Erklärung zum Datenschutz: 13.12.2024
+    </p>  
     <hr />
 
 
